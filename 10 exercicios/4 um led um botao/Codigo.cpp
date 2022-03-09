293f49b10daf162d6a771a8f1b4ff93e5dddf22f
Cavaleiro <99335851+Ninjahui@users.noreply.github.com>
2022-03-09 12:29:58 -0300

    Add files via upload

---
+++ 10 exercicios/4 um led um botao/Codigo.cpp
@@ -0,0 +1,18 @@
+  const int buttonPin = 2;
+
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop()
+{
+
+  
+  if(digitalRead(buttonPin) == HIGH){
+ 	digitalWrite(1, HIGH);
+  }else{
+   digitalWrite(1, LOW);
+  }
+}
